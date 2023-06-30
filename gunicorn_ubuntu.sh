2023104dac7ec5b179ef52a369a6a00c55710ca5
Kiran Madabhushi <50590709+kiranvaltix@users.noreply.github.com>
2023-06-30 08:06:41 -0700

    Update gunicorn_ubuntu.sh

--- gunicorn_ubuntu.sh
+++ gunicorn_ubuntu.sh
@@ -1,5 +1,7 @@
+# To run the app using gunicorn directly without going through nginx, to simulate longer time outs etc
 systemctl stop nginx
+systemctl stop restapp
 cd /svc
 CERT=/etc/ssl/certs/ssl-cert-snakeoil.pem
 KEY=/etc/ssl/private/ssl-cert-snakeoil.key
-gunicorn --certfile $CERT --keyfile $KEY -w 5 restapp:app --bind 0.0.0.0:443 --access-logfile /dev/stdout -t 0 -d
+gunicorn --certfile $CERT --keyfile $KEY -w 5 restapp:app --bind 0.0.0.0:443 --access-logfile /dev/stdout -t 0 -D
